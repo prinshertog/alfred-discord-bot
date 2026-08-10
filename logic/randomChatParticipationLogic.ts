aef6b1f144d47caf5566e3dc56ae7f7998513148
Jan Deploige <jan@deploige.be>
2026-08-10 11:23:57 +0200

    Change random messages logic

--- logic/randomChatParticipationLogic.ts
+++ logic/randomChatParticipationLogic.ts
@@ -1,7 +1,9 @@
 import { Message } from "discord.js";
 import { getRandomInt } from "../lib/helper.js";
 import { logMessage } from "../lib/log.js";
+import data from "../data/randomMessages.json" with {type: "json"};
 
+const randomMessages = data.messages;
 const componentName = "randomChatParticipationLogic"
 
 export function randomChatParticipationLogic(message: Message) {
@@ -9,13 +11,8 @@ export function randomChatParticipationLogic(message: Message) {
   const guildName: string = message.guild?.name ?? "Not found";
   const random: number = getRandomInt(1, 100);
   if (random === 1) {
-    message.reply("Gay");
-    logMessage("Random message was sent to a user!", componentName, guildName);
-  } else if (random === 2) {
-    message.reply("UwU");
-    logMessage("Random message was sent to a user!", componentName, guildName);
-  } else if (random === 3) {
-    message.reply("https://klipy.com/gifs/fire-writing-9");
+    const randomForMessage = getRandomInt(0, 2);
+    message.reply(randomMessages[randomForMessage]);
     logMessage("Random message was sent to a user!", componentName, guildName);
   }
 }
